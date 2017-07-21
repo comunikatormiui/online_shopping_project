4ce5d0a70596e6c90fdd90e60d8f062d1ab4e4e2
Jianglin Fu <jfa49@sfu.ca>
2017-07-21 02:11:14 -0700

    use fara's login/signup code to merge with james' navi bar

---
+++ online-shopping/controllers/passport.js
@@ -0,0 +1,75 @@
+
+var LocalStrategy   = require('passport-local').Strategy;
+
+module.exports = function(passport, User) {
+
+   
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
+    });
+
+    // used to deserialize the user
+    passport.deserializeUser(function(id, done) {
+        User.findById(id, function(err, user) {
+            done(err, user);
+        });
+    });
+
+   
+    passport.use('local-signup', new LocalStrategy({
+        usernameField : 'email',
+        passwordField : 'password',
+        passReqToCallback : true // allows us to pass back the entire request to the callback
+    },
+    function(req, email, password, done) {
+        var fname = req.body.fname;
+        var lname = req.body.lname;
+
+        process.nextTick(function() {
+            User.findOne({ 'local.email' :  email }, function(err, user) {
+                if (err)
+                    return done(err);
+                if (user) {
+                    return done(null, false, req.flash('signupMessage', 'The email already exists.'));
+                } 
+                else {
+                    var newUser  = new User();
+                    newUser.local.fname = fname;
+                    newUser.local.lname = lname;
+                    newUser.local.email    = email;
+                    newUser.local.password = newUser.generateHash(password);
+                    newUser.save(function(err) {
+                        if (err)
+                            throw err;
+                        return done(null, newUser);
+                    });
+                }
+
+            });    
+
+        });
+
+    }));
+
+    passport.use('local-login', new LocalStrategy({
+            usernameField : 'email',
+            passwordField : 'password',
+            passReqToCallback : true 
+        },
+        function(req, email, password, done) { 
+            User.findOne({ 'local.email' :  email }, function(err, user) {
+                if (err)
+                    return done(err);
+
+                if (!user)
+                    return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
+
+                if (!user.validPassword(password))
+                    return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
+
+                return done(null, user);
+            });
+
+        }
+    ));
+};
---
+++ online-shopping/models/user.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+
+var userSchema = mongoose.Schema({
+    local            	: {
+        email           : {type: String, required: true },
+        fname           : {type: String, required: true, max: 100},
+        lname           : {type: String, required: true, max: 100},
+        password     	: {type: String, required: true },
+        date_of_birth   : {type: Date},
+        address			: {type: String},
+        cell_phone 		: {type: String},
+    }
+});
+
+// generating a hash
+userSchema.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+// checking if password is valid
+userSchema.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.local.password);
+};
+
+// create the model for users and expose it to our app
+module.exports = mongoose.model('User', userSchema);
