72cd01fe82923502e37ab51dede019de478555db
Joshua Pryde <joshp@sfu.ca>
2017-07-21 19:16:08 -0700

    mid chat integration

--- chat-app/index.js
+++ chat-app/index.js
@@ -19,6 +19,6 @@
       });
     });
     // Listen application request on port 3000
-    http.listen(3000, function(){
-      console.log('listening on *:3000');
+    http.listen(3001, function(){
+      console.log('listening on *:3001');
     });
