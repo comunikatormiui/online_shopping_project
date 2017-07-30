7ef2630909b4d5d61f01e6334acb283b3ef97574
Jianglin Fu <jfa49@sfu.ca>
2017-07-29 21:19:37 -0700

    add slider at homepage. need to improve the appearence

---
+++ online-shopping/public/js/index.js
@@ -0,0 +1,13 @@
+/*
+ *
+ */
+
+/*global jQuery*/
+jQuery(document).ready(function ($) {
+
+  'use strict';
+
+  // This is stupid. We shouldn't have to do this!
+  $('[data-toggle="popover"]').popover();
+
+});
