dce454cae6c60206a2b96e59bd0a9e48c30289e9
Jianglin Fu <jfa49@sfu.ca>
2017-08-08 22:14:35 -0700

    last time

--- online-shopping/controllers/config.js
+++ online-shopping/controllers/config.js
@@ -3,21 +3,21 @@ module.exports = {
 		clientID: '798231580356363' ,
 		clientSecret: '09b4c5ec595b1746b1f44f21fc717aff' ,
 		callbackURL: 'http://localhost:3000/auth/facebook/callback'
-	},
+	},*/
 	github: {
 		clientID: '4d6b77439cce6eea959f',
 		clientSecret: '746a27e92a0a74a39a99d25c25b7942abbe9a5d1',
-		callbackURL: 'http://localhost:3000/auth/github/callback'
-	},*/
+		callbackURL: 'http://localhost:8000/auth/github/callback'
+	},
 	//for vagrant version
 	facebook: {
 		clientID: '1404935769542854' ,
 		clientSecret: '127989e8dbaab25fe9188d66b86399c8' ,
 		callbackURL: 'http://localhost:8000/auth/facebook/callback'
 	},
-	github: {
+	/*github: {
 		clientID: 'Iv1.dc584d799983a57e' || '199216a5a104204bccfe',
 		clientSecret: '15ae03bd9bd8726f749497faadefa2e343cd4b03' || '5d0efa59c4fcaf86594189c62fc4aa6b6bc5b179',
 		callbackURL: 'http://localhost:8000/auth/github/callback'
-	},
+	},*/
 }
